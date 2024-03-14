d3670f5c8e3a9dc3926e04024948f4d49b06385e
JuanBerrocal <juan.brudner@gmail.com>
2024-03-14 05:46:50 +0100

    Laboratorio 03

---
+++ Laboratorio03/Ejercicio01/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+/*=============================================================================
+ * PURPOSE: Cretaes an array of books, and given the title searchs the book
+ * in the array an returns whether the book is read or not.
+ * Also throws a custom exception when the given title is not found.
+ * ==========================================================================*/
+
+namespace Ejercicio01
+{
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public bool IsRead { get; set; }
+
+    }
+    public class BookNotFoundException : Exception {
+        private string message;
+        public BookNotFoundException(string messageP)
+        {
+            message = messageP;
+        }
+        public string getMessage()
+        {
+            return message;
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Book[] books = new Book[]  {
+                new Book {
+                    Title = "Don Quijote de la Mancha",
+                    IsRead = true
+                },
+                new Book {
+                    Title = "Fundacion",
+                    IsRead = false
+                },
+                new Book {
+                    Title = "El señor de los anillos",
+                    IsRead = false
+                },
+                new Book {
+                    Title = "Cien años de soledad",
+                    IsRead = true
+                },
+                new Book {
+                    Title = "El barón rampante",
+                    IsRead = false
+                },
+            };
+
+            bool IsBookRead(Book[] booksP, string titleP)
+            {
+                for (int i = 0; i < booksP.Length; i++)
+                {
+                    if (booksP[i].Title == titleP)
+                    {
+                        return booksP[i].IsRead;
+                    }
+                }
+                throw new BookNotFoundException($"El libro {titleP} no esta en la lista");
+            }
+
+            Console.WriteLine("Recorre una serie de libros e indica si se han leido o no.");
+
+            string[] targets = {"Fundacion", "La historia interminable", "Cien años de soledad", "El barón rampante"};
+
+            foreach (var target in targets)
+            {
+                try
+                {
+                    bool read = IsBookRead(books, target);
+
+                    Console.WriteLine($"El libro {target} {(read ? ("si") : ("no"))} se ha leido.");
+                }
+                catch (BookNotFoundException ex) {
+                    Console.WriteLine(ex.getMessage());
+                }
+                
+            }
+            
+
+        }
+    }
+}
---
+++ Laboratorio03/Ejercicio02/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Ejercicio02
+{
+    public class SlotMachine
+    {
+        public bool Roulette1 { get; set; }
+        public bool Roulette2 { get; set; }
+        public bool Roulette3 { get; set; }
+
+        public int Coins { get; set; }
+
+        public SlotMachine() {
+            Roulette1 = false;
+            Roulette2 = false;
+            Roulette3 = false;
+            Coins = 0;
+        }
+
+        public void play() {
+            var generator = new Random();
+
+            Coins++;
+            Roulette1 = (generator.Next(2) == 0);
+            Roulette2 = (generator.Next(2) == 0);
+            Roulette3 = (generator.Next(2) == 0);
+            Console.WriteLine(
+                $"{(Roulette1 ? "$" : "o")} " +
+                $"- {(Roulette2 ? "$" : "o")} " +
+                $"- {(Roulette3 ? "$" : "o")}");
+
+            if (Roulette1 && Roulette2 && Roulette3)
+            {
+                Console.WriteLine($"Congratulations!! You won {Coins} coins.");
+                Coins = 0;
+            }
+            else
+            {
+                Console.WriteLine("Good luck next time!!");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool finished = false;
+            SlotMachine machine = new SlotMachine();
+
+            Console.WriteLine("Slot Machine Game.");
+            Console.WriteLine("Want to play? (y/n)");
+            finished = (Console.ReadLine() != "y");
+
+            while (!finished)
+            {
+                machine.play();
+                Console.WriteLine("Another round? (y/n)");
+                finished = (Console.ReadLine() != "y");
+            }
+            Console.WriteLine("Thank you. See you next time.");
+        }
+    }
+}
